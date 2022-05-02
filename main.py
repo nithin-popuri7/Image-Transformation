4ce0a937a532c13a739b987d1e618aae042340da
Popuri Nithin <94154780+nithin-popuri7@users.noreply.github.com>
2022-05-02 17:45:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,94 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+inputImage=cv2.imread("huski.jpg")
+inputImage=cv2.cvtColor(inputImage, cv2.COLOR_BGR2RGB)
+plt.axis("off")
+plt.imshow(inputImage)
+plt.show()
+rows, cols, dim = inputImage.shape
+M= np.float32([[1, 0, 100],
+                [0, 1, 200],
+                 [0, 0, 1]])
+translatedImage =cv2.warpPerspective (inputImage, M, (cols, rows))
+plt.imshow(translatedImage)
+plt.show()
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+inputImage=cv2.imread("car.jpg")
+inputImage=cv2.cvtColor(inputImage, cv2.COLOR_BGR2RGB)
+plt.axis("off")
+plt.imshow(inputImage)
+plt.show()
+rows, cols, dim = inputImage.shape
+M = np. float32 ([[1.5, 0 ,0],
+                 [0, 1.8, 0],
+                  [0, 0, 1]])
+scaledImage=cv2.warpPerspective(inputImage, M, (cols * 2, rows * 2))
+plt.imshow(scaledImage)
+plt.show()
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+inputImage=cv2.imread("car.jpg")
+inputImage=cv2.cvtColor(inputImage, cv2.COLOR_BGR2RGB)
+plt.axis("off")
+plt.imshow(inputImage)
+plt.show()
+rows, cols, dim = inputImage.shape
+matrixX = np.float32([[1, 0.5, 0],
+                      [0, 1 ,0],
+                      [0, 0, 1]])
+
+matrixY = np.float32([[1, 0, 0],
+                      [0.5, 1, 0],
+                      [0, 0, 1]])
+shearedXaxis = cv2.warpPerspective (inputImage, matrixX, (int(cols * 1.5), int (rows * 1.5)))
+shearedYaxis = cv2.warpPerspective (inputImage, matrixY, (int (cols * 1.5), int (rows * 1.5)))
+plt.imshow(shearedXaxis)
+plt.show()
+plt.imshow(shearedYaxis)
+plt.show()
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+inputImage=cv2.imread("ducati.jpg")
+inputImage=cv2.cvtColor(inputImage, cv2.COLOR_BGR2RGB)
+plt.axis("off")
+plt.imshow(inputImage)
+plt.show()
+rows, cols, dim = inputImage.shape
+matrixx=np.float32([[1, 0, 0],
+                    [0,-1,rows],
+                    [0,0,1]])
+matrixy=np.float32([[-1, 0, cols],
+                    [0,1,0],
+                    [0,0,1]])
+reflectedX=cv2.warpPerspective(inputImage, matrixx, (cols, rows))
+reflectedY=cv2.warpPerspective(inputImage, matrixy, (cols, rows))
+plt.imshow(reflectedY)
+plt.show()
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+angle=np.radians(45)
+inputImage=cv2.imread("ducati.jpg")
+M=np.float32([[np.cos(angle),-(np.sin(angle)),0],
+               [np.sin(angle),np.cos(angle),0],
+               [0,0,1]])
+rotatedImage = cv2.warpPerspective(inputImage,M,(int(cols),int(rows)))
+plt.axis('off')
+plt.imshow(rotatedImage)
+plt.show()
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+angle=np.radians(45)
+inputImage=cv2.imread("huski.jpg")
+CroppedImage= inputImage[20:150, 60:230]
+plt.axis('off')
+plt.imshow(CroppedImage)
+plt.show()
+
